2f900786bb0417dbf8bffcaca20d83710d9bdd6a
AlfredoVargasG <alfvargas@alumnos.uai.cl>
2025-07-19 19:08:25 -0400

    feat: refactor lego route and add search route

--- backend/index.js
+++ backend/index.js
@@ -1,38 +1,85 @@
-const express = require('express');
-const pool = require('./db-config');
-const { createPagination } = require('./pagination');
+const express = require("express");
+const pool = require("./db-config");
+const { createPagination } = require("./pagination");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/lego', async(req, res) => {
-    try {
-        const { page, pageSize } = req.query;
+app.get("/lego", async (req, res) => {
+  try {
+    const { page, pageSize } = req.query;
 
-        if (!page || !pageSize) {
-            return res.status(404).send({ message: 'Faltan elementos a la consulta', data: [] });
-        }
-
-        const offset = (page -1) * pageSize;
-
-        const query = 'SELECT * FROM lego LIMIT $1 OFFSET $2';
-        const result = await pool.query(query, [pageSize, offset]);
-
-        if (result.rows.length === 0) {
-            return res.status(404).send({ message: 'Legos no encontrados', data: [] });
-        }
-
-        const pagination = await createPagination(pool, page, pageSize);
-
-        res.status(200).send({ message: 'Legos encontrados', data: result.rows, pagination })
-    } catch (error) {
-        console.error('Error in get route:', error);
-        res.status(500).send({ message: 'Internal Server Error' });
+    if (!page || !pageSize) {
+      return res.status(400).send({
+        message: "Faltan elementos a la consulta",
+        data: [],
+      });
     }
+
+    const offset = (page - 1) * pageSize;
+
+    const dataQuery = "SELECT * FROM lego LIMIT $1 OFFSET $2";
+    const countQuery = "SELECT COUNT(*) FROM lego";
+
+    const [dataResult, countResult] = await Promise.all([
+      pool.query(dataQuery, [pageSize, offset]),
+      pool.query(countQuery),
+    ]);
+
+    const totalLegos = parseInt(countResult.rows[0].count);
+
+    res.status(200).send({
+      message: dataResult.rows.length
+        ? "Legos encontrados"
+        : "Legos no encontrados",
+      data: dataResult.rows,
+      pagination: {
+        page: parseInt(page),
+        pageSize: parseInt(pageSize),
+        totalLegos,
+        totalPages: Math.ceil(totalLegos / pageSize),
+      },
+    });
+  } catch (error) {
+    console.error("Error in get route:", error);
+    res.status(500).send({ message: "Internal Server Error" });
+  }
+});
+
+app.get("/search/:column/:value", async (req, res) => {
+  try {
+    const { column, value } = req.params;
+    const { page, pageSize } = req.query;
+
+    if (!column || !value || !page || !pageSize) {
+      return res
+        .status(400)
+        .send({ message: "Faltan valores a la consulta", data: [] });
+    }
+
+    const offset = (page - 1) * pageSize;
+
+    const query = `SELECT * FROM lego WHERE ${column} = $1 LIMIT $2 OFFSET $3`;
+    const result = await pool.query(query, [value, pageSize, offset]);
+
+    res.status(200).send({
+      message: "Legos encontrados",
+      data: result.rows,
+      pagination: {
+        page,
+        pageSize,
+        totalLegos: result.rows.length,
+        totalPages: Math.ceil(result.rows.length / pageSize),
+      },
+    });
+  } catch (error) {
+    console.error("Error in search route:", error);
+    res.status(500).send({ message: "Internal Server Error:", error });
+  }
 });
 
 app.listen(PORT, () => {
-    console.log(`Server running in http://localhost:3000`)
-})
+  console.log(`Server running in http://localhost:3000`);
+});
