c5d73e25a2573fe8329665ab5b9cf8770c749ee4
AlfredoVargasG <alfvargas@alumnos.uai.cl>
2025-07-19 22:03:10 -0400

    feat: add legoService

--- frontend/src/app/home/home.ts
+++ frontend/src/app/home/home.ts
@@ -1,11 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { LegoService } from '../lego.service';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
 export class Home {
-
+  page: number = 1;
+  pageSize: number = 10;
+  legoData: any = [];
+  
+  constructor(private legoService: LegoService) {}
 }
